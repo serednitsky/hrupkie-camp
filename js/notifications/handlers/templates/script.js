a292b3c3479fdddc6a114cba8b110aca5d5e9820
TCSBANK\n.serednitskiy <n.serednitskiy@cp.ru>
2023-08-08 23:11:20 +0300

    minor fixes

--- js/notifications/handlers/templates/script.js
+++ js/notifications/handlers/templates/script.js
@@ -3,12 +3,13 @@ let password = document.getElementById('password')
 let notification = document.getElementById('notification')
 
 function checkPassword() {
-  if (password.value === 'dqqq') {
-    setTimeout("location.replace('https://hrupkie.ru/wp-content/uploads/BF_HRUPKIE_USTAV.pdf')", 1500);
+
+  if (password.value === (Math.random()*100000000000000000).toString()) {
+    setTimeout("location.assign('https://google.com')", 1500);
     notification.classList.add('hidden')
 
   } else {
-    setTimeout("location.replace('https://youtu.be/dQw4w9WgXcQ')", 1500);
+    setTimeout("location.assign('https://youtu.be/dQw4w9WgXcQ')", 1500);
     notification.classList.remove('hidden')
   }
 }
