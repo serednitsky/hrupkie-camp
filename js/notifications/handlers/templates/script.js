26cd05a3bddfac5fb159bffd0b448f2fcbe07d5b
serednitsky <62668481+serednitsky@users.noreply.github.com>
2023-08-10 18:56:48 +0300

    js fix

--- js/notifications/handlers/templates/script.js
+++ js/notifications/handlers/templates/script.js
@@ -3,12 +3,10 @@ let password = document.getElementById('password')
 let notification = document.getElementById('notification')
 
 function checkPassword() {
-
   if (password.value === atob('MTQyMDE5MTYxODIyMjU=')) {
     password.value = ''
     setTimeout("location.assign(atob('aHR0cHM6Ly9ocnVwa2llLnJ1L2hydXBraWUtY2FtcC1zdWNjZXNzLz91dG1fc291cmNlPWNhbXAmdXRtX21lZGl1bT1nYW1lJnV0bV9jYW1wYWlnbj1jb3JyZWN0'))", 1000);
     notification.classList.add('hidden')
-
   } else {
     password.value = ''
     setTimeout("location.assign(atob('aHR0cHM6Ly9ocnVwa2llLnJ1L2hydXBraWUtY2FtcC1mYWlsLz91dG1fc291cmNlPWNhbXAmdXRtX21lZGl1bT1nYW1lJnV0bV9jYW1wYWlnbj1pbmNvcnJlY3Q='))", 1000);
